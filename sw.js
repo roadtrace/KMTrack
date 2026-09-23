0897888d4b7f5968313fb52e95fb627380a35375
roadtrace <mmatriano19@gmail.com>
2026-09-23 09:43:55 +0800

    bumped sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // since the app already manages its own offline copy of that file and
 // the in-app "Reload dataset" button needs a real network attempt.
 
-const CACHE_VERSION = 'v196'; // bump this string whenever you deploy changes, to force an update
+const CACHE_VERSION = 'v197'; // bump this string whenever you deploy changes, to force an update
 const CACHE_NAME = `spotit-shell-${CACHE_VERSION}`;
 
 const APP_SHELL = [
